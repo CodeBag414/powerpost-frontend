1d22f664a134e504a4b9813778a1d4958189c120
Alex Elgin <sascha.elgin@gmail.com>
2017-02-26 14:05:05 +0600

    add a scratch of the Connection Control Bar component

--- src/App/views/Connections/index.js
+++ src/App/views/Connections/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-import Button from 'App/shared/atm.Button';
+import ConnectionsControlBar from './views/ConnectionsControlBar';
 
 class Connections extends React.Component {
     constructor(props) {
@@ -9,20 +9,9 @@ class Connections extends React.Component {
     }
 
     render() {
-        const styles = require('./styles.scss');
-
         return (
             <div className="container">
-                <div className="row">
-                    <div
-                        className={ ['col-xs-12', 'col-sm-6', 'col-md-5', 'col-lg-4', styles.noLeftPadding].join(' ') }>
-                        <h3 className={ [styles.noMargin].join(' ') }>Connected Accounts</h3>
-                    </div>
-                    <div
-                        className={ ['col-xs-12', 'col-sm-4', 'col-md-4', 'col-lg-3', styles.noLeftPadding].join(' ') }>
-                        <Button label="Connect a New Channel" secondary={ true }/>
-                    </div>
-                </div>
+                <ConnectionsControlBar />
             </div>
         );
     }
