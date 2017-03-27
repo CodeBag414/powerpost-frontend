b8baa7c4bd651a58bf07736d5a209cabdd418151
ch <codebag2345@gmail.com>
2017-03-28 02:40:10 +0800

    fix TestTextField

--- src/App/shared/atm.TestTextField/index.js
+++ src/App/shared/atm.TestTextField/index.js
@@ -1,29 +1,33 @@
 import React, { PropTypes } from 'react';
 
 const TextField = (props) => {
-    const styles = require('./styles.scss');
-    const errorStyles = props.errorText ? styles.errorBackground : '';
-    
-    return(
-        <div className={styles.textFieldContainer }>
-            <label className={styles.labelStyles }>{ props.floatingLabelText }</label>
-            <div className={ styles.inputContainer } >
-                <input className={ props.iconClass ? styles.inputStylesWithIcon : styles.inputStyles + ' ' + errorStyles } placeholder={ props.hintText } onChange={ props.onChange } />
-                { props.iconClass && <i className={ props.iconClass } /> }
-            </div>
-            <div className={ styles.errorContainer }>
-                { props.errorText }
-            </div>
-        </div>
-    ); 
+  const styles = require('./styles.scss');
+  const errorStyles = props.errorText ? styles.errorBackground : '';
+
+  return (
+    <div className={styles.textFieldContainer} style={props.style}>
+      <label className={styles.labelStyles}>{props.floatingLabelText}</label>
+      <div className={styles.inputContainer}>
+        <input className={props.iconClass ? styles.inputStylesWithIcon : styles.inputStyles + ' ' + errorStyles} type={props.type} name={props.name} value={props.value} placeholder={props.hintText} onChange={props.onChange} />
+        {props.iconClass && <i className={props.iconClass} />}
+      </div>
+      <div className={styles.errorContainer}>
+        {props.errorText}
+      </div>
+    </div>
+  );
 };
 
 TextField.PropTypes = {
-    floatingLabelText: PropTypes.string,
-    hintText: PropTypes.string,
-    onChange: PropTypes.func,
-    iconClass: PropTypes.string,
-    errorText: PropTypes.string
+  floatingLabelText: React.PropTypes.string,
+  type: React.PropTypes.string,
+  name: React.PropTypes.string,
+  value: React.PropTypes.any,
+  style: React.PropTypes.object,
+  hintText: React.PropTypes.string,
+  onChange: React.PropTypes.func,
+  iconClass: React.PropTypes.string,
+  errorText: React.PropTypes.string,
 };
 
 export default TextField;
