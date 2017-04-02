1f72599bb9340f2c196522d6b28960f383acc6b8
Bruno Sandvik <brunosandvik@gmail.com>
2017-04-03 04:42:28 +0900

    [349] import moment and populate months/year

--- src/App/views/Start/views/Checkout/index.js
+++ src/App/views/Start/views/Checkout/index.js
@@ -2,26 +2,38 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { Link } from 'react-router';
+import moment from 'moment';
+import { range } from 'lodash';
 
 import PPInput from 'App/shared/atm.Input';
 import PPDropdown from 'App/shared/atm.Dropdown';
 import PPRaisedButton from 'App/shared/atm.RaisedButton';
 
-const months = [
-  { value: '1', label: '01 - January' },
-  { value: '2', label: '02 - February' },
-  { value: '3', label: '03 - March' },
-  { value: '4', label: '04 - April' }
-];
+const monthOptions = moment.months().map((month, index) => {
+    let mm = index + 1;
+    if (mm >= 10) {
+        mm = '' + mm;
+    } else {
+        mm = '0' + mm;
+    }
+
+    return { value: index + 1, label: `${mm} - ${month}` };
+});
+const yearOptions = range(2018, 2033).map(year => ({
+    value: year,
+    label: year,
+}));
 
 class Checkout extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            expirationYear: '',
-            expirationYearError: '',
-            expirationMonth: '1',
-            expirationMonthError: '',
+            expirationYear: 2018,
+            expirationMonth: 1,
+            cardNumberValue: '',
+            cardNumberError: '',
+            cvcValue: '',
+            cvcError: ''
         };
     }
     
@@ -38,13 +50,39 @@ class Checkout extends Component {
         this.setState({ expirationYear: value });
     }
 
+    onCardNumberChange = (value) => {
+        this.setState({ cardNumberValue: value });
+    }
+
+    onCvcChange = (value) => {
+        this.setState({ cvcValue: value });
+    }
+
     render() {
         return (
             <div>
                 in signup view
                 <Link to="/start">Back to login</Link>
                 <form onSubmit={ this.onFormSubmit } >
-                    <PPDropdown auto error={ this.state.expirationMonthError } value={ this.state.expirationMonth } source={months} onChange={ this.onMonthChange }/>
+                    <div className="row">
+                        Credit Card Expiration Date
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-12 col-md-6">
+                            <PPDropdown auto value={ this.state.expirationMonth } source={monthOptions} onChange={ this.onMonthChange } />
+                        </div>
+                        <div className="col-sm-12 col-md-6">
+                            <PPDropdown auto value={ this.state.expirationYear } source={yearOptions} onChange={ this.onYearChange } />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col-sm-12">
+                            <PPInput type="number" error={ this.state.cardNumberError } value={ this.state.cardNumberValue } label="Credit Card #" onChange={ this.onCardNumberChange } />
+                        </div>
+                        <div className="col-sm-12">
+                            <PPInput type="number" error={ this.state.cvcError } value={ this.state.cvcValue } label="CVC #" onChange={ this.onCvcChange }/>
+                        </div>
+                    </div>
                     
                     <PPRaisedButton type="submit" label="Sign Up" primary={ true } />
                 </form>
