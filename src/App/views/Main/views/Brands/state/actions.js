9c756d7e35b280fb981514a042e6e18ea0beddad
dragon-webdev <proweb.dev28@gmail.com>
2017-04-04 20:23:09 +0200

    [251] remove prev files for refactor

--- src/App/views/Main/views/Brands/state/actions.js
+++
@@ -1,8 +0,0 @@
-import {
-    TOGGLE_ADD_BRAND_DIALOG,
-} from './constants';
-
-export function toggleDialog(dialogShown) {
-    console.log('add action', dialogShown)
-    return {type: TOGGLE_ADD_BRAND_DIALOG, dialogShown};
-}
