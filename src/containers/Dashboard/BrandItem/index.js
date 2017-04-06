acb63cd6b4cc47c793bed833906f124864d39843
Code Bag <codebag2345@gmail.com>
2017-04-06 11:30:02 +0800

    [333] brands part with eslint style

--- src/containers/Dashboard/BrandItem/index.js
+++ src/containers/Dashboard/BrandItem/index.js
@@ -3,39 +3,36 @@ import React from 'react';
 import styles from './styles.scss';
 
 class BrandItem extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-  	const inline = {
-  		brand: {
-  			display: 'inline-block',
-  		},
-  		socialButton: {
-  			float: 'right',
-  			color: '#6F6F6F',
-  		},
-  	}
+    const brand = this.props.brand;
 
-  	return (
-  		<div className="col-md-12">
-  			<div style={inline.brand}>
-	  			<img src="" alt="brand"/>
-	  			<span>
-	  				Role
-	  			</span>
-  			</div>
+    const thumbURL = (brand && brand.properties.thumb_url) ? brand.properties.thumb_url : '';
+    const groupTitle = brand && brand.group_title ? brand.group_title : '';
+    const title = brand && brand.title ? brand.title : '';
 
-  			<div style={inline.socialButton}>
-  				<i className="material-icons">send</i>
-  				<i className="material-icons">repeat</i>
-  				<i className="material-icons">group</i>
-  				<i className="material-icons">settings</i>
-  			</div>
-  		</div>
-  	);
+    return (
+      <div className={styles.brandItem}>
+        <div className={styles.brand}>
+          <img src={thumbURL} alt="Brand" />
+          <div className="role">
+            <p><b>{title}</b></p>
+            <p>{groupTitle}</p>
+          </div>
+        </div>
+
+        <div className={styles.socialButton}>
+          <i className="material-icons">send</i>
+          <i className="material-icons">repeat</i>
+          <i className="material-icons">group</i>
+          <i className="material-icons">settings</i>
+        </div>
+      </div>
+    );
   }
 }
+
+BrandItem.propTypes = {
+  brand: React.PropTypes.object,
+};
 
 export default BrandItem;
--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -3,21 +3,23 @@
  *
  *
  */
-
-import React from 'react';
-
-import {connect} from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-
-import PPButton from 'elements/atm.Button';
-import Avatar from 'elements/atm.Avatar';
-import withReactRouter from 'elements/hoc.withReactRouter';
-
-import { UserCanAccount } from 'config.routes/UserRoutePermissions';
+import React, { PropTypes, Component } from 'react';
 
 import {
+  connect,
+} from 'react-redux';
+import {
+  createStructuredSelector,
+} from 'reselect';
+
+import PPButton from 'elements/atm.Button';
+import withReactRouter from 'elements/hoc.withReactRouter';
+import {
+  UserCanAccount,
+} from 'config.routes/UserRoutePermissions';
+import {
   makeSelectUser,
-  makeSelectUserAccount,
+  makeSelectSharedAccounts,
 } from 'containers/App/selectors';
 
 import BrandItem from './BrandItem';
@@ -25,102 +27,103 @@ import styles from './styles.scss';
 
 const ReactRouterButton = withReactRouter(PPButton);
 
-class Dashboard extends React.Component {
-	constructor(props) {
-		super(props);
+class Dashboard extends Component {
+  static propTypes = {
+    user: PropTypes.object,
+    brands: PropTypes.any,
+  }
 
-	}
-
-	render() {
-		const userInfo = this.props.user || null;
-		const avatarUrl = (userInfo && userInfo.properties && userInfo.properties.thumb_url) ? userInfo.properties.thumb_url : null;
+  render() {
+    const brands = this.props.brands || null;
+    const userInfo = this.props.user || null;
+    const avatarUrl = (userInfo && userInfo.properties && userInfo.properties.thumb_url) ? userInfo.properties.thumb_url : null;
 
     return (
-    	<div className="container">
-    		<row>
-    			<div className="col-md-12">
-    				{
-    					userInfo && userInfo.display_name
-    					?	'Hello there ' + userInfo.display_name + ' !'
-    					: ''
-    				}
-    			</div>
-    		</row>
+      <div className="container">
+        <row>
+          <div className="col-md-12">
+            {
+              userInfo && userInfo.display_name
+              ?  'Hello there ' + userInfo.display_name + ' !'
+              : ''
+            }
+          </div>
+        </row>
 
-    		<row>
-    			<div className="col-md-7">
-    				<div className={['col-md-12', styles.userDashboardContain].join(' ')}>
-	    				<row>
-	    					
-	    					<h3><i className="material-icons">flash_on</i>My Brands</h3>
-	    				</row>
+        <row>
+          <div className="col-md-7">
+            <div className={['col-md-12', styles.userDashboardContain].join(' ')}>
+              <row>
+                <h3><i className="material-icons">flash_on</i>My Brands</h3>
+              </row>
 
-	    				<hr />
+              <hr />
 
-	    				<row>
-	    					<p>Easily jump into a brand to manage its posts.</p>
-	    					<div>
-	    						<BrandItem />
-	    						<BrandItem />
-	    						<BrandItem />
-	    					</div>
-	    				</row>
-	    			</div>
-    			</div>
-
-    			<div className="col-md-5">
-    				<div className={['col-md-12', styles.userDashboardContain].join(' ')}>
-	    				<row>
-	    					<h3><i className="material-icons">flash_on</i>My User Settings</h3>
-	    				</row>
-
-	    				<hr />
-
-	    				<row>
-	    					<div style={{margin: '20px 0'}}>
-	  							<p style={{display: 'inline-block', marginRight: '10px'}}>Go here to manage all of your user info.</p>
-	  							<ReactRouterButton
-	  								style={{fontSize: '10px'}}
-	  								label="Go to Settings"
-	  								primary
-	  								to={'/user/settings'}
-	  							/>
-  							</div>
-	    				</row>
-
-	    				<row>
-	    					<div className={styles.profile}>
-	                <img
-	                  src={avatarUrl}
-	                  className={styles.avatar}
-	                  alt="Profile Photo"
-	                />
-
-	                <div className={styles.userInfo}>
-		                <div>
-		                	<p>Name</p>
-		                	<span> {userInfo.display_name || ''} </span>
-		                </div>
-
-		                <div>
-		                	<p>Email</p>
-		                	<span> {userInfo.email || ''} </span>
-		                </div>
-	                </div>
+              <row>
+                <p>Easily jump into a brand to manage its posts.</p>
+                <div style={ {padding: '10px 0 50px'} }>
+                  {
+                    brands
+                    ? brands.map((brand, index) => <BrandItem brand={brand} key={index} />)
+                    : null
+                  }
                 </div>
-	    				</row>
-    				</div>
-    			</div>
+              </row>
+            </div>
+          </div>
 
-    		</row>
-    	</div>
-    	);
+          <div className="col-md-5">
+            <div className={['col-md-12', styles.userDashboardContain].join(' ')}>
+              <row>
+                <h3><i className="material-icons">flash_on</i>My User Settings</h3>
+              </row>
+
+              <hr />
+
+              <row>
+                <div style={ {margin: '20px 0'} }>
+                  <p style={ {display: 'inline-block', marginRight: '10px'} }>Go here to manage all of your user info.</p>
+                  <ReactRouterButton
+                    style={ {fontSize: '10px'} }
+                    label="Go to Settings"
+                    primary
+                    to={'/user/settings'}
+                  />
+                </div>
+              </row>
+
+              <row>
+                <div className={styles.profile}>
+                  <img
+                    src={avatarUrl}
+                    className={styles.avatar}
+                    alt="Profile"
+                  />
+
+                  <div className={styles.userInfo}>
+                    <div>
+                      <p>Name</p>
+                      <span> {userInfo.display_name || ''} </span>
+                    </div>
+
+                    <div>
+                      <p>Email</p>
+                      <span> {userInfo.email || ''} </span>
+                    </div>
+                  </div>
+                </div>
+              </row>
+            </div>
+          </div>
+        </row>
+      </div>
+    );
   }
 }
 
-
 const mapStateToProps = createStructuredSelector({
   user: makeSelectUser(),
+  brands: makeSelectSharedAccounts(),
 });
 
 export default UserCanAccount(connect(mapStateToProps)(Dashboard));
