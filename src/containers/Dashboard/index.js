f41d6b6957b640011889e6a771be9380e91d4440
Code Bag <codebag2345@gmail.com>
2017-04-07 23:06:27 +0800

    [333] user dashboard background image

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -39,85 +39,88 @@ class Dashboard extends Component {
     const avatarUrl = (userInfo && userInfo.properties && userInfo.properties.thumb_url) ? userInfo.properties.thumb_url : null;
 
     return (
-      <div className="container">
-        <row>
-          <div className="col-md-12">
-            <h4>
-              {
-                userInfo && userInfo.display_name
-                ?  'Hello there ' + userInfo.display_name + ' !'
-                : ''
-              }
-            </h4>
-          </div>
-        </row>
-
-        <row>
-          <div className="col-md-7">
-            <div className={['col-md-12', styles.userDashboardContain].join(' ')}>
-              <row>
-                <h3><i className="fa fa-bolt" style={{marginRight: '15px'}}></i>My Brands</h3>
-              </row>
-
-              <hr />
-
-              <row>
-                <p>Easily jump into a brand to manage its posts.</p>
-                <div style={ {padding: '10px 0 50px'} }>
-                  {
-                    brands
-                    ? brands.map((brand, index) => <BrandItem brand={brand} key={index} />)
-                    : null
-                  }
-                </div>
-              </row>
+      <div>
+        <div className={styles.bgImage}></div>
+        <div className="container">
+          <row>
+            <div className="col-md-12">
+              <h4>
+                {
+                  userInfo && userInfo.display_name
+                  ?  'Hello there ' + userInfo.display_name + ' !'
+                  : ''
+                }
+              </h4>
             </div>
-          </div>
+          </row>
 
-          <div className="col-md-5">
-            <div className={['col-md-12', styles.userDashboardContain].join(' ')}>
-              <row>
-                <h3><i className="fa fa-bolt" style={{marginRight: '15px'}}></i>My User Settings</h3>
-              </row>
+          <row>
+            <div className="col-md-7">
+              <div className={['col-md-12', styles.userDashboardContain].join(' ')}>
+                <row>
+                  <h3><i className="fa fa-bolt" style={{marginRight: '15px'}}></i>My Brands</h3>
+                </row>
 
-              <hr />
+                <hr />
 
-              <row>
-                <div style={ {margin: '20px 0'} }>
-                  <p style={ {display: 'inline-block', marginRight: '10px'} }>Go here to manage all of your user info.</p>
-                  <ReactRouterButton
-                    style={ {fontSize: '10px'} }
-                    label="Go to Settings"
-                    primary
-                    to={'/user/settings'}
-                  />
-                </div>
-              </row>
+                <row>
+                  <p>Easily jump into a brand to manage its posts.</p>
+                  <div style={ {padding: '10px 0 50px'} }>
+                    {
+                      brands
+                      ? brands.map((brand, index) => <BrandItem brand={brand} key={index} />)
+                      : null
+                    }
+                  </div>
+                </row>
+              </div>
+            </div>
 
-              <row>
-                <div className={styles.profile}>
-                  <img
-                    src={avatarUrl}
-                    className={styles.avatar}
-                    alt="Profile"
-                  />
+            <div className="col-md-5">
+              <div className={['col-md-12', styles.userDashboardContain].join(' ')}>
+                <row>
+                  <h3><i className="fa fa-bolt" style={{marginRight: '15px'}}></i>My User Settings</h3>
+                </row>
 
-                  <div className={styles.userInfo}>
-                    <div>
-                      <p>Name</p>
-                      <span> {userInfo.display_name || ''} </span>
-                    </div>
+                <hr />
 
-                    <div>
-                      <p>Email</p>
-                      <span> {userInfo.email || ''} </span>
+                <row>
+                  <div style={ {margin: '20px 0'} }>
+                    <p style={ {display: 'inline-block', marginRight: '10px'} }>Go here to manage all of your user info.</p>
+                    <ReactRouterButton
+                      style={ {fontSize: '10px'} }
+                      label="Go to Settings"
+                      primary
+                      to={'/user/settings'}
+                    />
+                  </div>
+                </row>
+
+                <row>
+                  <div className={styles.profile}>
+                    <img
+                      src={avatarUrl}
+                      className={styles.avatar}
+                      alt="Profile"
+                    />
+
+                    <div className={styles.userInfo}>
+                      <div>
+                        <p>Name</p>
+                        <span> {userInfo.display_name || ''} </span>
+                      </div>
+
+                      <div>
+                        <p>Email</p>
+                        <span> {userInfo.email || ''} </span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </row>
+                </row>
+              </div>
             </div>
-          </div>
-        </row>
+          </row>
+        </div>
       </div>
     );
   }
