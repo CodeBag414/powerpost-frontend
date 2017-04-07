6bf4692f7b51c4050f87f41f08ed2e913a9938ab
dragon-webdev <proweb.dev28@gmail.com>
2017-04-07 22:00:55 +0200

    [251] add FilePicker component in AddBrandDialog component

--- src/containers/Brands/AddBrandDialog/index.js
+++ src/containers/Brands/AddBrandDialog/index.js
@@ -1,46 +1,206 @@
-import React, { PropTypes } from 'react';
-import { Tab, Tabs } from 'react-toolbox';
+import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import PPDialog from 'elements/atm.Dialog';
+import Dialog from 'react-toolbox/lib/dialog';
+import FlatButton from 'material-ui/FlatButton';
+import filepicker from 'filepicker-js';
+
 import PPInput from 'elements/atm.Input';
 import TextField from 'elements/atm.TextField';
 import PPButton from 'elements/atm.Button';
+import Avatar from 'elements/atm.Avatar';
 
+import { updateRequest } from 'containers/App/actions';
+import {
+  makeSelectUser,
+  makeSelectUserAccount,
+  makeSelectFilePickerKey,
+} from 'containers/App/selectors';
+
+import dialogtheme from './dialog-styles.scss';
 import styles from './styles.scss';
 
 class AddBrandDialog extends React.Component {
+  static propTypes = {
+    user: PropTypes.object,
+    userOwnAccount: PropTypes.object,
+    filePickerKey: PropTypes.string,
+    update: PropTypes.func,
+
+    dialogShown: PropTypes.bool,
+    handleDialogToggle: PropTypes.func,
+  }
+
   constructor(props) {
     super(props);
 
+    this.openFilePicker = this.openFilePicker.bind(this);
+    this.profileUpdate = this.profileUpdate.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+    const user = this.props.user || null;
+    const userProperties = (user && user.properties) || null;
+    const userOwnAccount = this.props.userOwnAccount || null;
+    const userOwnAccountProperties = (userOwnAccount && userOwnAccount.properties) || null;
+
     this.state = {
-      index: 1,
-      fixedIndex: 1,
-      inverseIndex: 1,
+      avatar: (userProperties && userProperties.thumb_url) || '',
+      avatarKey: '',
+      name: user.display_name || '',
+      title: (userOwnAccount && userOwnAccount.title) || '',
     };
   }
 
-  getChannelClass(connection) {
-    return Object.prototype.hasOwnProperty.call(styles, connection.channel) ? styles[connection.channel] : '';
+  openFilePicker() {
+    this.setState({
+      open: false,
+    });
+
+    const tthis = this;
+    filepicker.setKey(this.props.filePickerKey);
+
+    const filePickerOptions = {
+      cropRatio: 1 / 1,
+      buttonText: 'Choose',
+      container: 'modal',
+      multiple: false,
+      maxFiles: 1,
+      imageQuality: 80,
+      imageMax: [1200, 1200],
+      services: ['CONVERT', 'COMPUTER', 'WEBCAM', 'IMAGE_SEARCH', 'FLICKR', 'GOOGLE_DRIVE', 'FACEBOOK', 'INSTAGRAM', 'BOX', 'SKYDRIVE', 'URL'],
+      conversions: ['crop', 'filter'],
+    };
+
+    const fileStoreOptions = {
+      location: 'S3',
+    };
+
+    const uploadSuccess = function (Blobs) {
+      tthis.setState({
+        avatar: Blobs[0].url,
+        avatarKey: Blobs[0].key,
+      });
+    };
+
+    const uploadFail = function () {
+      tthis.setState({
+        avatarKey: '',
+      });
+    };
+
+    const uploadProgress = function (progress) {
+      console.log(JSON.stringify(progress));
+    };
+
+    filepicker.pickAndStore(
+      filePickerOptions,
+      fileStoreOptions,
+      uploadSuccess,
+      uploadFail,
+      uploadProgress,
+    );
+  }
+
+  profileUpdate(event) {
+    event.preventDefault();
+
+    const data = {
+      avatarKey: this.state.avatarKey,
+      title: this.state.title,
+      accountID: this.props.userOwnAccount.account_id,
+    };
+    console.log('update data', data)
+    return;
+    this.props.update(data);
+  }
+
+  handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    this.setState({
+      [name]: value,
+    });
   }
 
   render() {
     const labelActions = [
-            { label: 'Cancel', onClick: this.props.handleDialogToggle },
-            { label: 'Save', onClick: this.props.handleDialogToggle },
+      { label: 'Cancel', onClick: this.props.handleDialogToggle }
     ];
+
+    const inline = {
+      avatar: {
+        position: 'relative',
+        top: '-20px',
+        width: '186px',
+        display: 'inline-block',
+        verticalAlign: 'top'
+      },
+      avatarImg: {
+        left: '0px',
+        width: '180px',
+        height: '180px',
+        borderRadius: '0',
+      },
+      avatardescription: {
+        display: 'inline-block',
+        width: '350px',
+        fontSize: '16px',
+        color: '#999',
+        lineHeight: '20px',
+        margin: '0px 0px 0px 30px',
+        verticalAlign: 'top'
+      }
+    };
+
+    console.log('dialog user', this.props.user)
+    console.log('dialog userOwnAccount', this.props.userOwnAccount)
+
     return (
-      <PPDialog title="CREATE NEW BRAND" active={this.props.dialogShown} actions={labelActions}>
-        <PPInput type="text" label="Enter the brand title" name="brandtitle" maxLength={50} />
-        <PPButton icon="add" label="CREATE" raised accent />
-      </PPDialog>
+      <Dialog title="Add New Brand" active={this.props.dialogShown} actions={labelActions} theme={ dialogtheme }>
+        <form onSubmit={this.profileUpdate}>
+          <row>
+            <div>
+              <TextField type="text"  name="title" floatingLabelText="Brand Name" maxLength={100} 
+                value={this.state.title} onChange={this.handleChange} style={{ margin: 0 }} />
+            </div>
+          </row>
+          <row>
+            <div>
+              <h5 style={{ marginLeft: '0px', color: '#9d9d9d' }}>Brand Icon</h5>
+              <br />
+              <div style={inline.avatar}>
+                <Avatar image={this.state.avatar} style={inline.avatarImg} theme={styles} 
+                  onClick={this.openFilePicker} />
+                <FlatButton label="Update Brand Icon" className={styles.updateAvatar} 
+                  onClick={this.openFilePicker} />
+              </div>
+              <div style={inline.avatardescription}>
+                Managing multiple brands can be hard. Personalize your new brand with its own image or logo to help!
+              </div>
+            </div>
+          </row>
+          <row>
+            <div>
+              <PPButton type="submit" label="Add Brand" primary={!false} className={styles.submit} />
+            </div>
+          </row>
+        </form>
+      </Dialog>
     );
   }
 }
 
-AddBrandDialog.propTypes = {
-  socialUrls: PropTypes.object,
-  dialogShown: PropTypes.bool,
-  handleDialogToggle: PropTypes.func,
-};
+export function mapDispatchToProps(dispatch) {
+  return {
+    update: (data) => dispatch(updateRequest(data)),
+  };
+}
 
-export default AddBrandDialog;
+const mapStateToProps = createStructuredSelector({
+  user: makeSelectUser(),
+  userOwnAccount: makeSelectUserAccount(),
+  filePickerKey: makeSelectFilePickerKey(),
+});
+
+export default (connect(mapStateToProps, mapDispatchToProps)(AddBrandDialog));
