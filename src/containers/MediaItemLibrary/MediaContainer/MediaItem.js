eb5c301b34eb2a7b8ec44c4323b156026f67ebc9
Philip <philip.cressler@powerpost.digital>
2017-04-17 16:10:41 +0000

    stash changes to pull from dev

--- src/containers/MediaItemLibrary/MediaContainer/MediaItem.js
+++ src/containers/MediaItemLibrary/MediaContainer/MediaItem.js
@@ -5,18 +5,20 @@ import styled from 'styled-components';
 const MediaItem = (props) => {
   console.log(props.mediaItem);
   return(
-    <div className='row'>
-      <div className='col-md-5'>
-       
-      </div>
-      <div className='col-md-2'>
-       
-      </div>
-      <div className='col-md-3'>
-        { props.mediaItem.creation_time }
-      </div>
-      <div className='col-md-2'>
-        
+    <div>
+      <div className='row' style={{ height: '60px'}}>
+        <div className='col-md-5' style={{ height: '100%'}}>
+          <span style={{lineHeight: '60px'}}><img src={props.mediaItem.properties.thumb_url || ''} style={{ width: '45px', height: '45px' }}/>{props.mediaItem.properties.description}</span>
+        </div>
+        <div className='col-md-2' style={{ height: '100%'}}>
+          <span style={{lineHeight: '60px'}} >{props.mediaItem.type}</span>
+        </div>
+        <div className='col-md-3' style={{ height: '100%'}}>
+          <span style={{lineHeight: '60px'}}>{props.mediaItem.creation_time}</span>
+        </div>
+        <div className='col-md-2' style={{ height: '100%'}}>
+          <span style={{lineHeight: '60px'}}>{props.mediaItem.user_display_name}</span>
+        </div>
       </div>
       <hr />
     </div>
