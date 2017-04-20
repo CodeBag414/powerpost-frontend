f2a117e6663a5d2c79c7eca67664b817dd900a03
Bruno Sandvik <brunosandvik@gmail.com>
2017-04-21 02:03:56 +0900

    [358] fix get started link

--- src/containers/Login/Topbar/index.js
+++ src/containers/Login/Topbar/index.js
@@ -7,7 +7,7 @@ import theme from 'theme';
 import Wrapper from './Wrapper';
 
 const Topbar = () => (<Wrapper color={theme.textColor}>
-  Don&#39;t have an account? <Link className="" to="/signup/account?PP01-PRO-MO" style={{ marginLeft: '40px' }}><SimpleButton color={theme.textColor}>Get Started</SimpleButton></Link>
+  Don&#39;t have an account? <Link className="" to="/signup/account?plan_id=PP01-PRO-MO" style={{ marginLeft: '40px' }}><SimpleButton color={theme.textColor}>Get Started</SimpleButton></Link>
 </Wrapper>);
 
 export default Topbar;
