97cd1afb38a2d01d24064bdad6c42b43e6c08b4e
Bruno Sandvik <brunosandvik@gmail.com>
2017-04-21 02:02:54 +0900

    [358] cleanup  login page

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -8,24 +8,31 @@ import Wrapper from './Wrapper';
 import Topbar from './Topbar';
 import FormWrapper from './FormWrapper';
 
-export default class Login extends React.Component {
-  static propTypes = {
-    children: PropTypes.node,
-  }
+const Login = ({ children }) => (
+  <Wrapper>
+    <LeftPane signup={false}>
+      <img src={imgLogo} alt="Logo" />
+      <div style={{ position: 'absolute', left: 0, bottom: '80px' }}>
+        <div style={{ fontSize: '2rem' }}>Power Your Knowledge</div>
+        <div style={{ marginTop: '10px', fontSize: '3.5rem', lineHeight: 1.1 }}>3 Ways to User a GoPro for Content Creation</div>
+        <div style={{ marginTop: '15px', fontSize: '1.5rem' }}>
+          Want to learn how it can work for you, too?
+          <br /><br />
+          Read this article
+        </div>
+      </div>
+    </LeftPane>
+    <RightPane>
+      <Topbar />
+      <FormWrapper>
+        { children }
+      </FormWrapper>
+    </RightPane>
+  </Wrapper>
+);
 
-  render() {
-    const { children } = this.props;
-    return (
-      <Wrapper>
-        <LeftPane>
-        </LeftPane>
-        <RightPane>
-          <Topbar />
-          <FormWrapper>
-            { children }
-          </FormWrapper>
-        </RightPane>
-      </Wrapper>
-    );
-  }
-}
+Login.propTypes = {
+  children: PropTypes.node,
+};
+
+export default Login;
