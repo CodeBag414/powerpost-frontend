cf7330567a7a71db1c4a88a76747cc266da80df1
Seiji Takahashi <seijitakahashi0619@gmail.com>
2017-05-23 06:31:25 +0900

    [377] Add Channels & Times content

---
+++ src/containers/PostEditor/Channels/ChannelSlots/ChannelSlotTimestamp.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import moment from 'moment';
+
+import PPButton from 'elements/atm.Button';
+
+function ChannelSlotTimestamp({ post, handleClickTimestamp, currentPost }) {
+  return (
+    <div className="slot-timestamp">
+      <PPButton className={currentPost === post && 'active'} onClick={() => handleClickTimestamp(post)}>
+        {moment.unix(post.get('schedule_time')).format('MMMM D, YYYY [at] hh:mma')}
+      </PPButton>
+    </div>
+  );
+}
+
+ChannelSlotTimestamp.propTypes = {
+  post: ImmutablePropTypes.map,
+  currentPost: ImmutablePropTypes.map,
+  handleClickTimestamp: PropTypes.func,
+};
+
+export default ChannelSlotTimestamp;
