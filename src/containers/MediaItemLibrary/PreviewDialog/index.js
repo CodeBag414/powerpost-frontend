19d536bd21f6ee78974b5c3baeaf98b10fac7d56
Alex Wu <alex.wustar@gmail.com>
2017-05-28 03:29:18 +0800

    [BUG-113] Update Preview Dialog

--- src/containers/MediaItemLibrary/PreviewDialog/index.js
+++ src/containers/MediaItemLibrary/PreviewDialog/index.js
@@ -1,28 +1,73 @@
 import React from 'react';
-import PPDialog from 'elements/atm.Dialog';
-import Wrapper from './Wrapper';
-import Button from 'elements/atm.Button';
+import PropTypes from 'prop-types';
+import { isEmpty } from 'lodash';
+
+import theme from 'theme';
+
 import Preview from 'components/Preview';
 
-const LinkDialog = (props) => {
-  
-  return(
+import PPDialog from 'elements/atm.Dialog';
+import FontIcon from 'elements/atm.FontIcon';
+import Button from 'elements/atm.Button';
+import SimpleButton from 'elements/atm.SimpleButton';
+
+import Wrapper from './Wrapper';
+
+function PreviewDialog({ previewDialog, closeAllDialog, mediaItem }) {
+  const mediaType = mediaItem.type;
+  let iconName = '';
+
+  switch (mediaType) {
+    case 'link':
+      iconName = 'fa-link';
+      break;
+    case 'video':
+      iconName = 'fa-video-camera';
+      break;
+    case 'image':
+      iconName = 'fa-picture-o';
+      break;
+    case 'file':
+      iconName = 'fa-text-o';
+      break;
+    default:
+      iconName = '';
+      break;
+  }
+
+  return (
     <PPDialog
-      active={props.previewDialog}
-      title='Preview'
-      onEscKeyDown={props.closeAllDialog}
-      onOverlayClick={props.closeAllDialog}
-      actions={props.actions}
+      active={previewDialog}
+      onEscKeyDown={closeAllDialog}
+      onOverlayClick={closeAllDialog}
     >
-      <Wrapper>
-        <Preview item={props.mediaItem} />
-        <div style={{textAlign: 'right'}}>
-          <Button label='Close preview' onClick={props.closeAllDialog} style={{margin: '5px'}} />
-          <Button label='Add to Post' icon='add' primary style={{margin: '5px'}} />
+      {!isEmpty(mediaItem.properties) && <Wrapper>
+        <div className="header-wrapper">
+          <h3><i className={`fa ${iconName}`} />{mediaType}</h3>
+          <button onClick={closeAllDialog}><FontIcon value="clear" /></button>
+        </div>
+        <Preview item={mediaItem} />
+        <div style={{ textAlign: 'right', marginTop: '20px' }}>
+          <SimpleButton
+            style={{ fontSize: '13px', marginRight: '20px' }}
+            color={theme.textColor}
+            onClick={closeAllDialog}
+            noBorder
+          >
+            Close Preview
+          </SimpleButton>
+          <Button label="Add to Post" icon="add" primary style={{ margin: '5px' }} />
         </div>
       </Wrapper>
-    </PPDialog>  
+      }
+    </PPDialog>
   );
+}
+
+PreviewDialog.propTypes = {
+  previewDialog: PropTypes.bool,
+  mediaItem: PropTypes.shape(),
+  closeAllDialog: PropTypes.func,
 };
 
-export default LinkDialog;
+export default PreviewDialog;
