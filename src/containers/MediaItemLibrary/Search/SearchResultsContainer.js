8580d6862ea240b96408f20e4ee64165df9f46a5
Alex Wu <alex.wustar@gmail.com>
2017-05-29 16:08:15 +0800

    [BUG-113] Cleanup SearchContainer

--- src/containers/MediaItemLibrary/Search/SearchResultsContainer.js
+++ src/containers/MediaItemLibrary/Search/SearchResultsContainer.js
@@ -1,9 +1,14 @@
-import React from 'react';
 import styled from 'styled-components';
 
 const SearchResultsContainer = styled.div`
   overflow: auto;
   clear: both;
+  .loading {
+    text-align: center;
+    img {
+      width: 80px;
+    }
+  }
 `;
 
 export default SearchResultsContainer;
