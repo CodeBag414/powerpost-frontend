1be9e42579c0ac05edb7a2fa73b8f307079b780f
Alex Wu <alex.wustar@gmail.com>
2017-05-31 22:04:30 +0800

    [BUG-113] Fix Sort function

--- src/containers/MediaItemLibrary/MediaContainer/index.js
+++ src/containers/MediaItemLibrary/MediaContainer/index.js
@@ -1,19 +1,35 @@
-import { connect } from 'react-redux';
-import MediaContainer from './MediaContainer';
+import React, { Component } from 'react';
 import { createStructuredSelector } from 'reselect';
+import { connect } from 'react-redux';
+
+import Wrapper from './Wrapper';
+import MediaItem from './MediaItem';
+import ProcessingItem from './ProcessingItem';
+
 import {
   makeSelectVisibleMediaItemsWithSearch,
-  makeSelectMediaItemsSorted,
   makeSelectProcessingItem,
+  makeSelectFilter,
+  makeSelectSortOrder,
 } from '../selectors';
 
 const mapStateToProps = createStructuredSelector({
-  mediaItems: makeSelectMediaItemsSorted(),
   visibleMediaItems: makeSelectVisibleMediaItemsWithSearch(),
   processingItem: makeSelectProcessingItem(),
+  filter: makeSelectFilter(),
+  sortOrder: makeSelectSortOrder(),
 });
 
-const VisibleMediaContainer = connect(
-  mapStateToProps)(MediaContainer);
-  
-export default VisibleMediaContainer;
+class VisibleMediaContainer extends Component {
+  render() {
+    return (
+      <Wrapper className='row'>
+        { !this.props.visibleMediaItems && <span>You don't have any media items yet.</span> }
+        { this.props.processingItem && <ProcessingItem />}
+        { this.props.visibleMediaItems && this.props.visibleMediaItems.map((item, i) => <MediaItem key={i} mediaItem={item} query={this.props.query} createPostSet={this.props.createPostSet} onDelete={this.props.onConfirmDelete} openPreview={this.props.openPreview} openEditor={this.props.openEditor} />)}
+      </Wrapper>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(VisibleMediaContainer);
