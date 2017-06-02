02bd54a16d31b604e74fe8e7fee46b6d22903855
Joris Smit <jorismit410@gamil.com>
2017-06-02 03:30:18 +0200

    [BUG-128] Update avatar in top nav

--- src/components/Nav/TopNav/Avatar.js
+++ src/components/Nav/TopNav/Avatar.js
@@ -1,15 +1,14 @@
-import React from 'react';
+
 import styled from 'styled-components';
 
 const Avatar = styled.div`
-  background-image: url(${(props) => props.avatarSrc});
-  background-size: cover;
   margin-top: 10px;
   margin-bottom: 10px;
   height: 40px;
   width: 40px;
   margin-right: 10px;
   border-radius: 4px;
+  cursor: pointer;
 `;
 
 export default Avatar;
