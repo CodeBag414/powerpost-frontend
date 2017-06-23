76451fbf7126147cd4f7f5e1dc3c3967e0555404
Bart Romanow <bartromanow@post.com>
2017-06-23 06:40:09 +0200

    vertical sort board

--- src/containers/App/reducer.js
+++ src/containers/App/reducer.js
@@ -33,6 +33,9 @@ import {
   FETCH_POST_SETS_BY_ST_SUCCESS,
   FETCH_POST_SETS_BY_ST_FAILURE,
   SET_POST_SETS_BY_ST,
+  FETCH_POST_SETS_BY_SO_REQUEST,
+  FETCH_POST_SETS_BY_SO_SUCCESS,
+  FETCH_POST_SETS_BY_SO_FAILURE,
   FETCH_GROUP_USERS,
   FETCH_GROUP_USERS_SUCCESS,
   FETCH_GROUP_USERS_ERROR,
@@ -78,6 +81,11 @@ const initialState = fromJS({
   inviteEmailToGroup: {},
   postSets: [],
   postSetsByST: {
+    requesting: true, // As soon as calendar view mounts, it starts loading. Maybe change later..
+    error: null,
+    data: null,
+  },
+  postSetsBySO: {
     requesting: true, // As soon as calendar view mounts, it starts loading. Maybe change later..
     error: null,
     data: null,
@@ -222,6 +230,20 @@ function globalReducer(state = initialState, action) {
       }));
     case SET_POST_SETS_BY_ST:
       return state.set('postSetsByST', action.postSetsByST);
+    case FETCH_POST_SETS_BY_SO_REQUEST:
+      return state.setIn(['postSetsBySO', 'requesting'], true);
+    case FETCH_POST_SETS_BY_SO_SUCCESS:
+      return state.set('postSetsBySO', fromJS({
+        requesting: false,
+        error: null,
+        data: action.postSets,
+      }));
+    case FETCH_POST_SETS_BY_SO_FAILURE:
+      return state.set('postSetsBySO', fromJS({
+        requesting: false,
+        error: action.error,
+        data: null,
+      }));
     case FETCH_GROUP_USERS:
       return state
         .set('groupUsers', {
@@ -315,11 +337,12 @@ function globalReducer(state = initialState, action) {
           ['postSetsByST', 'data', 'post_when_ready_post_sets'],
           (postSets) => postSets.filter((postSet) => postSet.get('post_set_id') !== action.id)
         )
-        .updateIn(['postSets'], (postSets) => postSets.filter((postSet) => postSet.get('post_set_id') !== action.id));
+        .updateIn(['postSets'], (postSets) => postSets.filter((postSet) => postSet.get('post_set_id') !== action.id))
+        .updateIn(['postSetsBySO', 'data', 'post_sets'], (postSets) => postSets.filter((postSet) => postSet.get('post_set_id') !== action.id));
     }
     case CHANGE_POST_SET_STATUS:
       return state
-        .updateIn(['postSets'], (postSets) => postSets.map((postSet) =>
+        .updateIn(['postSetsBySO', 'data', 'post_sets'], (postSets = fromJS([])) => postSets.map((postSet) =>
           postSet.get('post_set_id') !== action.id ? postSet : postSet.set('status', action.status)
         ));
     case FETCH_POSTS:
