d61e4c2ca01d38d162297c480fde5e1c17d4fc41
Seiji Takahashi <seijitakahashi0619@gmail.com>
2017-06-30 04:21:17 +0900

    Revert "[BUGS-413] Fix bug in updating post_set"
    This reverts commit 7330842ff3cd8b4205c474a7c33f8ae2988fa2fc.

--- src/containers/PostEditor/Content/index.js
+++ src/containers/PostEditor/Content/index.js
@@ -175,12 +175,10 @@ class Content extends Component {
 
   handleMessageBlur = (event, message = this.state.globalMessage) => {
     const { updatePostSet, postSet } = this.props;
-    const postType = postSet.getIn(['details', 'post_type']);
     updatePostSet({
       ...postSet.get('details').toJS(),
       id: postSet.getIn(['details', 'post_set_id']),
       message,
-      post_type: postType === 'document' ? 'file' : postType,
     });
   }
 
