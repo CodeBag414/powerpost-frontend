bf25c0e8d3e31a6dc5948107e0c6b564812fdef3
Seiji Takahashi <seijitakahashi0619@gmail.com>
2017-07-08 05:35:51 +0900

    [BUGS-419] Fix delete button position

--- src/containers/PostEditor/Sidebar/index.js
+++ src/containers/PostEditor/Sidebar/index.js
@@ -4,13 +4,14 @@ const Sidebar = styled.div`
   width: 258px;
   border-left: 1px solid #DBDFE0;
   .button-flat {
-    height: 30px;
-    color: #8C9496 !important;
-    font-size: 12px;
-    line-height: 1;
     border: none;
-    margin-top: 10px;
-    margin-left: 10px;
+    color: #8C9496 !important;
+    float: right;
+    font-size: 12px;
+    height: 30px;
+    line-height: 1;
+    margin-top: 14px;
+    margin-right: 10px;
     &:hover {
       background: inherit !important;
     }
