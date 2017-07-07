edb14aebced33951b38ce8591cf70cf421ad479f
Bruno Sandvik <brunosandvik@gmail.com>
2017-07-07 23:18:58 +0200

    [BUGS] fix 345

--- src/containers/Login/index.js
+++ src/containers/Login/index.js
@@ -12,13 +12,14 @@ const Login = ({ children }) => (
   <Wrapper>
     <LeftPane signup={false}>
       <img src={imgLogo} alt="Logo" />
-      <div style={{ position: 'absolute', left: 0, bottom: '80px' }}>
-        <div style={{ fontSize: '2rem' }}>Power Your Knowledge</div>
-        <div style={{ marginTop: '10px', fontSize: '3.5rem', lineHeight: 1.1 }}>3 Ways to User a GoPro for Content Creation</div>
+      <div style={{ position: 'absolute', left: 0, bottom: '160px' }}>
+        <div style={{ marginTop: '10px', fontSize: '3.5rem', lineHeight: 1.1 }}>
+          Join the Brand<br />
+          Publishing Revolution
+        </div>
         <div style={{ marginTop: '15px', fontSize: '1.5rem' }}>
-          Want to learn how it can work for you, too?
-          <br /><br />
-          Read this article
+          PowerPost Delivers Software And Services To<br />
+          Transform Your Brand Into A Power Publisher.
         </div>
       </div>
     </LeftPane>
