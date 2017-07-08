b38068bf4269a992ed6aba4c36d1492fd09860e5
Seiji Takahashi <seijitakahashi0619@gmail.com>
2017-07-08 11:50:41 +0900

    [BUGS-419] Show/hide of sidebar

--- src/containers/PostEditor/Sidebar/index.js
+++ src/containers/PostEditor/Sidebar/index.js
@@ -1,8 +1,14 @@
 import styled from 'styled-components';
 
 const Sidebar = styled.div`
-  width: 258px;
   border-left: 1px solid #DBDFE0;
+  position: absolute;
+  right: ${(props) => props.expanded ? '0' : '-258px'};
+  top: 0;
+  transition: right .25s;
+  width: 258px;
+  height: 100%;
+
   .button-flat {
     border: none;
     color: #8C9496 !important;
