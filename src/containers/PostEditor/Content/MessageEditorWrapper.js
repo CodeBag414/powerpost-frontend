62410c3c2502a43096cbec9b7d22724c6086ef02
clever2000 <brunosandvik@gmail.com>
2017-07-17 17:31:15 +0200

    Ppbugs (#474)
    * fix #567
    
    * hot fix for tightening up space between message and preview post

--- src/containers/PostEditor/Content/MessageEditorWrapper.js
+++ src/containers/PostEditor/Content/MessageEditorWrapper.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const MessageEditorWrapper = styled.div`
   position: relative;
-  margin-bottom: 60px;
+  margin-bottom: 40px;
 `;
 
 export default MessageEditorWrapper;
