CodeBag414/powerpost-frontend

commit eddf5b40014f84058ac8fedc08388ff94b6adde3

[BUGS-583] Fix the channel slot not adding bug (#493)

src/containers/PostEditor/Channels/AddChannelSlotDialog/index.js
@@ -36,6 +36,7 @@ class AddChannelSlotDialog extends Component {
   };
 
   componentWillReceiveProps = (nextProps) => {
+    if (this.state.channels.length) return;
     this.setState({
       channels: nextProps.connections
         .filter((connection) => connection.channel !== 'wordpress' && connection.status === '1')

src/containers/PostEditor/index.js
@@ -151,7 +151,6 @@ class PostEditor extends Component {
     deletePostSet(id);
 
     if (location && !location.pathname.endsWith('/posts')) {
-      console.log(location);
       goBack();
     }
   }

commit 7305e998f4fc585b4ac308870bacd957d7f9c0aa

[BUGS-583] Fix the bug introduced by eddf5b40 (#503)

src/containers/PostEditor/Channels/AddChannelSlotDialog/index.js
@@ -36,7 +36,7 @@ class AddChannelSlotDialog extends Component {
   };
 
   componentWillReceiveProps = (nextProps) => {
-    if (!this.state.channels.length) {
+    if (!this.state.channels.length || !this.props.active) {
       this.setState({
         channels: nextProps.connections
           .filter((connection) => connection.channel !== 'wordpress' && connection.status === '1')