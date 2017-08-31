d8852b8aab4db84be2ea32e58d17c4f786115bb4
Seiji Takahashi <seijitakahashi0619@gmail.com>
2017-08-31 20:50:16 +0900

    Redux refactoring: rename constants (#659)

--- src/containers/Statistics/ChannelsInfo/constants.js
+++ src/containers/Statistics/ChannelsInfo/constants.js
@@ -1,7 +1,7 @@
-export const IS_LOADING_CHANNEL = 'IS_LOADING_CHANNEL';
-export const FETCH_CHANNEL = 'FETCH_CHANNEL';
-export const FETCH_CHANNEL_SUCCESS = 'FETCH_CHANNEL_SUCCESS';
-export const FETCH_CHANNEL_ERR = 'FETCH_CHANNEL_ERR';
-export const FETCH_CHANNEL_CONNECTION = 'FETCH_CHANNEL_CONNECTION';
-export const IS_WEEKLY_ANALYTICS = 'IS_WEEKLY_ANALYTICS';
-export const IS_MONTHLY_ANALYTICS = 'IS_MONTHLY_ANALYTICS';
+export const IS_LOADING_CHANNEL = 'Statistics/ChannelsInfo/IS_LOADING_CHANNEL';
+export const FETCH_CHANNEL = 'Statistics/ChannelsInfo/FETCH_CHANNEL';
+export const FETCH_CHANNEL_SUCCESS = 'Statistics/ChannelsInfo/FETCH_CHANNEL_SUCCESS';
+export const FETCH_CHANNEL_ERR = 'Statistics/ChannelsInfo/FETCH_CHANNEL_ERR';
+export const FETCH_CHANNEL_CONNECTION = 'Statistics/ChannelsInfo/FETCH_CHANNEL_CONNECTION';
+export const IS_WEEKLY_ANALYTICS = 'Statistics/ChannelsInfo/IS_WEEKLY_ANALYTICS';
+export const IS_MONTHLY_ANALYTICS = 'Statistics/ChannelsInfo/IS_MONTHLY_ANALYTICS';
